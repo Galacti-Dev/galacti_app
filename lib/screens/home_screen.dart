54924458dc3307b737a22a0ed60f18ac4d9e2847
Galacti-Dev <galacti.dev@outlook.com>
2025-03-21 14:34:54 +0100

    iss łagodnie się porusza po ekranie

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -4,6 +4,7 @@ import 'package:vector_math/vector_math_64.dart' as vm;
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:sensors_plus/sensors_plus.dart';
+import 'dart:math'; // Dodaj import biblioteki dart:math
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -16,6 +17,9 @@ class _HomeScreenState extends State<HomeScreen> {
   CameraController? _cameraController;
   late List<CameraDescription> _cameras;
   List<CelestialObject> _celestialObjects = []; // Inicjalizacja pustej listy
+
+  List<vm.Vector3> _accelerometerReadings = [];
+  List<vm.Vector3> _magnetometerReadings = [];
 
   @override
   void initState() {
@@ -86,16 +90,87 @@ class _HomeScreenState extends State<HomeScreen> {
 
   void _listenToSensors() {
     accelerometerEvents.listen((AccelerometerEvent event) {
-      print('Accelerometer: $event');
-    });
-
-    gyroscopeEvents.listen((GyroscopeEvent event) {
-      print('Gyroscope: $event');
+      _accelerometerReadings.add(vm.Vector3(event.x, event.y, event.z));
+      if (_accelerometerReadings.length > 10) {
+        _accelerometerReadings.removeAt(0);
+      }
+      _processSensorData();
     });
 
     magnetometerEvents.listen((MagnetometerEvent event) {
-      print('Magnetometer: $event');
+      _magnetometerReadings.add(vm.Vector3(event.x, event.y, event.z));
+      if (_magnetometerReadings.length > 10) {
+        _magnetometerReadings.removeAt(0);
+      }
+      _processSensorData();
     });
+  }
+
+  void _processSensorData() {
+    if (_accelerometerReadings.isNotEmpty && _magnetometerReadings.isNotEmpty) {
+      final accelerometerVector = _averageVector(_accelerometerReadings);
+      final magnetometerVector = _averageVector(_magnetometerReadings);
+
+      final rotationMatrix =
+          _calculateRotationMatrix(accelerometerVector, magnetometerVector);
+      final eulerAngles = _getEulerAngles(rotationMatrix);
+
+      // Oblicz pozycję ISS na ekranie na podstawie kątów Eulera
+      final issPosition = vm.Vector3(
+        eulerAngles.y * 100,
+        eulerAngles.x * 100,
+        -10,
+      );
+
+      setState(() {
+        _celestialObjects = [
+          CelestialObject(
+            name: 'ISS',
+            position: issPosition,
+          ),
+        ];
+      });
+    }
+  }
+
+  vm.Matrix3 _calculateRotationMatrix(
+      vm.Vector3 accelerometer, vm.Vector3 magnetometer) {
+    final R = vm.Matrix3.zero();
+
+    final H = accelerometer.cross(magnetometer);
+    final normH = H.normalized();
+
+    final N = accelerometer.cross(normH);
+    final normN = N.normalized();
+
+    final normAccelerometer = accelerometer.normalized();
+
+    R.setColumn(0, normN);
+    R.setColumn(1, normH);
+    R.setColumn(2, normAccelerometer);
+
+    return R;
+  }
+
+  vm.Vector3 _getEulerAngles(vm.Matrix3 rotationMatrix) {
+    final pitch = asin(-rotationMatrix.entry(2, 0));
+    final roll = atan2(rotationMatrix.entry(2, 1), rotationMatrix.entry(2, 2));
+    final yaw = atan2(rotationMatrix.entry(1, 0), rotationMatrix.entry(0, 0));
+
+    return vm.Vector3(pitch, roll, yaw);
+  }
+
+  vm.Vector3 _averageVector(List<vm.Vector3> vectors) {
+    if (vectors.isEmpty) {
+      return vm.Vector3.zero();
+    }
+
+    final sum = vm.Vector3.zero();
+    for (final vector in vectors) {
+      sum.add(vector);
+    }
+
+    return sum / vectors.length.toDouble();
   }
 }
 
