0d7cdfb3c683b6969422f7ff5c4320eb2acb0407
Galacti-Dev <galacti.dev@outlook.com>
2025-03-21 14:09:23 +0100

    dodanie iss do kamery

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:camera/camera.dart'; // Import pakietu camera
+import 'package:camera/camera.dart';
+import 'package:vector_math/vector_math_64.dart' as vm;
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -11,11 +14,42 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   CameraController? _cameraController;
   late List<CameraDescription> _cameras;
+  List<CelestialObject> _celestialObjects = []; // Inicjalizacja pustej listy
 
   @override
   void initState() {
     super.initState();
     _setupCamera();
+    _fetchIssData();
+  }
+
+  Future<void> _fetchIssData() async {
+    final response =
+        await http.get(Uri.parse('http://api.open-notify.org/iss-now.json'));
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      final position = data['iss_position'];
+      setState(() {
+        _celestialObjects = [
+          CelestialObject(
+            name: 'ISS',
+            position: vm.Vector3(
+              double.parse(position['longitude']),
+              double.parse(position['latitude']),
+              -10,
+            ),
+          ),
+        ];
+      });
+    } else {
+      print('Failed to fetch ISS data');
+    }
+  }
+
+  @override
+  void dispose() {
+    _cameraController?.dispose();
+    super.dispose();
   }
 
   Future<void> _setupCamera() async {
@@ -28,12 +62,6 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   @override
-  void dispose() {
-    _cameraController?.dispose();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
     if (_cameraController == null || !_cameraController!.value.isInitialized) {
       return const Center(child: CircularProgressIndicator());
@@ -43,7 +71,39 @@ class _HomeScreenState extends State<HomeScreen> {
       appBar: AppBar(
         title: const Text('Mapa nieba'),
       ),
-      body: CameraPreview(_cameraController!),
+      body: Stack(
+        children: [
+          CameraPreview(_cameraController!),
+          ..._celestialObjects
+              .map((object) => CelestialObjectWidget(object: object))
+              .toList(),
+        ],
+      ),
+    );
+  }
+}
+
+class CelestialObject {
+  final String name;
+  final vm.Vector3 position;
+
+  CelestialObject({required this.name, required this.position});
+}
+
+class CelestialObjectWidget extends StatelessWidget {
+  final CelestialObject object;
+
+  const CelestialObjectWidget({super.key, required this.object});
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      left: object.position.x + MediaQuery.of(context).size.width / 2,
+      top: object.position.y + MediaQuery.of(context).size.height / 2,
+      child: Text(
+        object.name,
+        style: const TextStyle(color: Colors.white),
+      ),
     );
   }
 }
